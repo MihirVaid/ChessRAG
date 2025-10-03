1b3cc6b54c8c5ac737db65b8bf61c01d0208cfa0
MihirVaid <128417588+MihirVaid@users.noreply.github.com>
2025-10-03 23:26:00 +0530

    Update rag_agent.py

--- rag_agent.py
+++ rag_agent.py
@@ -1,10 +1,12 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_tool_calling_agent, AgentExecutor
-from retriever_tools import tools
 from retriever_tools import load_all_retriever_tools
-tools = load_all_retriever_tools()
+
 def init_rag_agent():
+    #Load retrievers fresh each time
+    tools = load_all_retriever_tools()
+
     llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
     prompt = ChatPromptTemplate.from_messages(
         [
@@ -15,6 +17,8 @@ def init_rag_agent():
             MessagesPlaceholder(variable_name="agent_scratchpad")
         ]
     )
+
+    #agent with fresh tools
     agent = create_tool_calling_agent(llm, tools, prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
